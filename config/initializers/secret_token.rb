3c533ca5fd7b22580efe8c04d7ea15c707130534
ivangranados <ivan.granados@koombea.com>
2013-04-18 21:41:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gcadmin::Application.config.secret_token = 'e688f0d09dabfcf6c93f07d0cbcff73b30f79d8f388f4e86712b6cfb385380c9fe6760674323a0b8246a7b6d7f97ca25a2edea25a218cb87dac39465b773549e'
